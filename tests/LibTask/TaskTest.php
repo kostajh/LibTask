46c6032be71a788e327bb0d19237a0f44b762cb0
Kosta Harlan <kostajh@gmail.com>
2013-10-02 12:03:44 -0400

    Build out tests for Task object

--- tests/LibTask/TaskTest.php
+++ tests/LibTask/TaskTest.php
@@ -27,8 +27,23 @@ class TaskTest extends  \PHPUnit_Framework_TestCase
         $task->setModified("20130831T165548Z");
         $task->setStatus("pending");
         $task->setUrgency("10.00");
+        $task->setProject('morning');
+        $task->setTags(array('coffee', 'life'));
+        $task->setPriority('H');
+        // Check if values are set.
         $this->assertRegExp('/Grind coffee beans/', $task->getDescription());
+        $this->assertContains('coffee', $task->getTags());
+        $this->assertContains('life', $task->getTags());
+        $this->assertEquals('pending', $task->getStatus());
+        $this->assertEquals('1377921600', $task->getDue());
+        $this->assertEquals('1377968148', $task->getEntry());
+        $this->assertEquals('1377968148', $task->getModified());
+        $this->assertEquals('morning', $task->getProject());
+        $this->assertEquals('10.00', $task->getUrgency());
+        $this->assertEquals('H', $task->getPriority());
+        // Test adding the task.
         $taskwarrior = new Taskwarrior($this->taskrc, $this->taskData);
         $result = $taskwarrior->addTask($task);
+        $this->assertEquals('1' ,$result['success']);
     }
 }
